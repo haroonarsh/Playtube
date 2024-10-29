fbd61da73fd01094ac6ba8d0418c5c0dc9ec0083
Haroon Arshad <arshadharoon216@gmail.com>
2024-10-29 22:39:01 +0500

    ApiError handlling

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.cors({
+app.use(cors({
     origin: process.env.PORT.CORS_ORIGIN,
-    Credentials: true
-})
+    credentials: true
+}))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
---
+++ src/utils/async Handler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((error) => next(error))
+    }
+}
+
+export { asyncHandler }
+
+
+          // hard order functions
+// const asyncHandler = () => {}
+// const asyncHandler = () => { () => {} }
+// const asyncHandler = () => () => {}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).jason({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
