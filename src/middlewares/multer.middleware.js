3bd7b1be51c7e655cf1eb42dc5006f4ccf7b6543
Haroon Arshad <arshadharoon216@gmail.com>
2024-11-03 00:05:02 +0500

    add setup configured

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer"
+
+
+        // define storage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/temp");
+    },
+    username: function (req, file, cb) {
+        cb(null, file.originalname);
+        console.log("file.originalname", file.originalname);
+    }
+})
+
+export const upload = multer({ storage });
