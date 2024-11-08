3bc0d875c33e2822781a27ac0a9cc297e8151dab
Haroon Arshad <arshadharoon216@gmail.com>
2024-11-08 22:31:13 +0500

    fix some bugs in controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -184,7 +184,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
 
     const incomingRefreshToken = req.cookie.refreshToken || req.body.refreshToken;
 
-    if (!incomingToken) {
+    if (!incomingRefreshToken) {
         throw new ApiError(401, "unauthorized request")
     }
 
