a4d3d7e3fa670d02214ea9cf1b1cc0687275f10b
Haroon Arshad <arshadharoon216@gmail.com>
2024-11-09 22:26:57 +0500

    fix some error in subscription

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -4,7 +4,7 @@ const subscriptionSchema = new mongoose.Schema({
     subscriber: {
         type: Schema.Types.ObjectId, // one who is subscribing
         ref: "User"
-    }
+    },
     channel: {
         type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
         ref: "User"
