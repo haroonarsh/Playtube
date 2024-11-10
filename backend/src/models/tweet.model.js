dfa7c4980018f5fdd09da4e1604c88cc5008e7d8
Haroon Arshad <arshadharoon216@gmail.com>
2024-11-10 22:21:15 +0500

    Add subscription and tweet functionality

---
+++ backend/src/models/tweet.model.js
@@ -0,0 +1,18 @@
+import mongoose, { Schema } from "mongoose";
+
+const tweetSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: [true, "Content is required"],
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+},
+    {
+    timestamps: true
+    }
+)
+
+export const Tweet = mongoose.model("Tweet", tweetSchema);
