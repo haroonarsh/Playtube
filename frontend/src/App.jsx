d37679ba8ec36c4057bfd60c0fa31f58658734b7
Haroon Arshad <arshadharoon216@gmail.com>
2024-11-17 21:48:09 +0500

    add all Router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,67 @@
 import { useEffect, useState } from 'react';
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import RegisterPage from './components/registerPage/RegisterPage';
+import LoginPage from './components/loginPage/LoginPage';
+import ForgotPassword from './components/forgotPassword/ForgotPassword';
+import VideoListViewPage from './components/videoListPage/VideoListViewPage';
+import VideoListCardViewPage from './components/videoListPage/VideoListCardViewPage';
 import HomePage from './components/homePage/HomePage';
+import EmptyPage from './components/MyChannel/EmptyPage';
+import UploadingModelPopup from './components/MyChannel/uploads/UploadingModelPopup';
+import UploadingVideomodelScrollPopup from './components/MyChannel/uploads/UploadingVideomodelScrollPopup';
+import UploadingSuccessModelPopup from './components/MyChannel/uploads/UploadingSuccessModelPopup';
 
+              // Router
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layouts />,
+//     children: [
+//       {
+//         path: "",
+//         element: <HomePage />,
+//       },
+//       {
+//         path: "/register",
+//         element: <RegisterPage />,
+//       },
+//       {
+//         path: "/login",
+//         element: <LoginPage />,
+//       },
+//       {
+//         path: "/forgot-password",
+//         element: <ForgotPassword />,
+//       },
+//       {
+//         path: "/home",
+//         element: <VideoListCardViewPage />,
+//       },
+//       {
+//         path: "/home/history",
+//         element: <VideoListViewPage />,
+//       },
+//       {
+//         path: "/home/channel",
+//         element: <EmptyPage />,
+//       },
+//       {
+//         path: "/home/channel/upload",
+//         element: <UploadingModelPopup />,
+//         children: {
+//           path: "/home/channel/upload/uploading",
+//           element: <UploadingVideomodelScrollPopup />,
+//         },
+//       },
+//         {
+//         path: "/home/channel/upload/success",
+//         element: <UploadingSuccessModelPopup />,
+//       }
+//     ]
+//   }
+  
+// ])
 function App() {
 
   const [data, setDate] = useState([]);
@@ -14,10 +74,23 @@ function App() {
   
 
   return (
-    <>
-    <HomePage />
-    </>
+   <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/register' element={<RegisterPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/home' element={<VideoListCardViewPage />} />
+      <Route path='/home/history' element={<VideoListViewPage />} />
+      <Route path='/home/channel' element={<EmptyPage />} />
+      <Route path='/home/channel/upload' element={<UploadingModelPopup />} />
+      <Route path='/home/channel/upload/uploading' element={<UploadingVideomodelScrollPopup />} />
+      <Route path='/home/channel/upload/success' element={<UploadingSuccessModelPopup />} />
+    </Routes>
+   </BrowserRouter>
   )
 }
 
 export default App
+
+
