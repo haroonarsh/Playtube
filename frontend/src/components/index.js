4ada58e6d13d90f465a65764b32fe7085ab9fbe3
Haroon Arshad <arshadharoon216@gmail.com>
2024-11-17 22:03:53 +0500

    create index file for store all file in one place

---
+++ frontend/src/components/index.js
@@ -0,0 +1,65 @@
+import ChannelEmptyPage from "./channelPages/ChannelEmptyPage";
+import channelVideosPage from "./channelPages/ChannelVideosPage";
+import EmptyPlayList from "./channelPlayList/EmptyPlayList";
+import PlayListPage from "./channelPlayList/PlayListPage";
+import PlayListVideoPage from "./channelPlayList/PlayListVideoPage";
+import ForgotPassword from "./forgotPassword/ForgotPassword";
+import HomePage from "./homePage/HomePage";
+import OldPage from "./homePage/OldPage";
+import LoginPage from "./loginPage/LoginPage";
+import AdminDashboard from "./MyChannel/admin/AdminDashboard";
+import EditChannelInfo from "./MyChannel/PersonalInfo/EditChannelInfo";
+import EditPassword from "./MyChannel/PersonalInfo/EditPassword";
+import EditPersonalInfo from "./MyChannel/PersonalInfo/EditPersonalInfo";
+import About from "./MyChannel/privacyPolicy/About";
+import TermCondition from "./MyChannel/privacyPolicy/TermCondition";
+import MyEmptyTweet from "./MyChannel/tweets/MyEmptyTweet";
+import Tweets from "./MyChannel/tweets/Tweets";
+import DeleteUploadedVideo from "./MyChannel/uploads/DeleteUploadedVideo";
+import EditUploadedVideo from "./MyChannel/uploads/EditUploadedVideo";
+import UploadingModelPopup from "./MyChannel/uploads/UploadingModelPopup";
+import UploadingSuccessModelPopup from "./MyChannel/uploads/UploadingSuccessModelPopup";
+import UploadingVideomodelScrollPopup from "./MyChannel/uploads/UploadingVideomodelScrollPopup";
+import EmptyPage from "./MyChannel/EmptyPage";
+import RegisterPage from "./registerPage/RegisterPage";
+import EmptySubscribed from "./subscribed/EmptySubscribed";
+import Subscribed from "./subscribed/Subscribed";
+import Channeltweet from "./tweetPages/Channeltweet";
+import EmptyTweet from "./tweetPages/EmptyTweet";
+import VideoListCardViewPage from "./videoListPage/VideoListCardViewPage";
+import VideoListViewPage from "./videoListPage/VideoListViewPage";
+import VideoPlayPage from "./videoPlayPage/VideoPlayPage";
+
+export {
+    ChannelEmptyPage,
+    channelVideosPage,
+    EmptyPlayList,
+    PlayListPage,
+    PlayListVideoPage,
+    ForgotPassword,
+    HomePage,
+    OldPage,
+    LoginPage,
+    AdminDashboard,
+    EditChannelInfo,
+    EditPassword,
+    EditPersonalInfo,
+    About,
+    TermCondition,
+    MyEmptyTweet,
+    Tweets,
+    DeleteUploadedVideo,
+    EditUploadedVideo,
+    UploadingModelPopup,
+    UploadingSuccessModelPopup,
+    UploadingVideomodelScrollPopup,
+    EmptyPage,
+    RegisterPage,
+    EmptySubscribed,
+    Subscribed,
+    Channeltweet,
+    EmptyTweet,
+    VideoListCardViewPage,
+    VideoListViewPage,
+    VideoPlayPage
+}
