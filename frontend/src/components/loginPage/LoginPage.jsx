e89520170d9fec1c1e0db2ba8bb95515d9ad442c
Haroon Arshad <arshadharoon216@gmail.com>
2024-11-17 21:53:45 +0500

    create login page

--- frontend/src/components/loginPage/LoginPage.jsx
+++ frontend/src/components/loginPage/LoginPage.jsx
@@ -1,81 +1,126 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 function LoginPage() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  
+  
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    axios.post('/api/v1/users/login', { email, password })
+    .then(response => {
+        // Handle successful login response
+        console.log(response.data);
+        if (response.data.message === 'User logged in successfully') {
+          toast.success("User logged in successfully !", {
+            position: "top-center"
+          })
+          navigate('/home'); // Navigate to homepage if login is successful
+      }
+        setEmail('');
+        setPassword('');
+
+    })
+    .catch(error => {
+        // Handle login error
+        console.error("user not found");
+        toast.error("User not found !", {
+          position: "top-center"
+        })
+        setEmail('');
+        setPassword('');
+      });
+  }
+
   return (
     <>
-    <link
-  rel="preload"
-  as="image"
-  href="https://images.pexels.com/photos/1144275/pexels-photo-1144275.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
-<div class="min-h-screen bg-[#121212]">
-  <div class="mx-auto flex w-full items-stretch justify-between gap-10">
-    <div class="mt-20 flex w-full flex-col items-start justify-start p-6 md:w-1/2 lg:px-10">
-      <div class="w-full">
-        <h1 class="mb-2 text-5xl font-extrabold text-white">Log in</h1>
-        <p class="text-xs text-slate-400">Before we start, please log into your account</p>
+
+<div className="min-h-screen bg-[#121212]">
+    <div className="mx-auto flex w-full items-stretch justify-between gap-10">
+    <div className="mt-20 flex w-full flex-col items-start justify-start p-6 md:w-1/2 lg:px-10">
+      <div className="w-full">
+        <h1 className="mb-2 text-5xl font-extrabold text-white">Log in</h1>
+        <p className="text-xs text-slate-400">Before we start, please log into your account</p>
       </div>
-      <div class="my-14 flex w-full flex-col items-start justify-start gap-4">
-        <div class="flex w-full flex-col items-start justify-start gap-2">
-          <label class="text-xs text-slate-200">Username</label>
+      <form className="my-14 flex w-full flex-col items-start justify-start gap-4">
+        <div className="flex w-full flex-col items-start justify-start gap-2">
+          <label className="text-xs text-slate-200">Email</label>
           <input
             placeholder="Enter a username..."
-            autocomplete="false"
-            class="w-full border-[1px] border-white bg-black p-4 text-white placeholder:text-gray-500" />
+            autoComplete="false"
+            className="w-full border-[1px] border-white bg-black p-4 text-white placeholder:text-gray-500" 
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+           />
         </div>
-        <div class="flex w-full flex-col items-start justify-start gap-2">
-          <label class="text-xs text-slate-200">Password</label>
+        <div className="flex w-full flex-col items-start justify-start gap-2">
+          <label className="text-xs text-slate-200">Password</label>
           <input
             placeholder="Enter a password..."
-            autocomplete="false"
+            autoComplete="false"
             type="password"
-            class="w-full border-[1px] border-white bg-black p-4 text-white placeholder:text-gray-500" />
+            className="w-full border-[1px] border-white bg-black p-4 text-white placeholder:text-gray-500" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            />
         </div>
-        <div class="inline-flex w-full items-center justify-between">
-          <div class="mr-4 flex items-center">
+        <div className="inline-flex w-full items-center justify-between">
+          <div className="mr-4 flex items-center">
             <input
               type="checkbox"
               id="checkbox-1"
-              class="absolute h-6 w-6 cursor-pointer opacity-0 [&:checked+div]:bg-green-500 [&:checked+div_svg]:block"
+              className="absolute h-6 w-6 cursor-pointer opacity-0 [&:checked+div]:bg-green-500 [&:checked+div_svg]:block"
               name="checkbox-1" />
-            <div class="mr-2 flex h-6 w-6 flex-shrink-0 items-center justify-center border-[1px] border-white bg-transparent focus-within:border-white">
+            <div className="mr-2 flex h-6 w-6 flex-shrink-0 items-center justify-center border-[1px] border-white bg-transparent focus-within:border-white">
               <svg
-                class="pointer-events-none hidden h-3 w-3 fill-current text-white"
+                className="pointer-events-none hidden h-3 w-3 fill-current text-white"
                 version="1.1"
                 viewBox="0 0 17 12"
                 xmlns="http://www.w3.org/2000/svg">
                 <g
                   fill="none"
-                  fill-rule="evenodd">
+                  fillRule="evenodd">
                   <g
                     transform="translate(-9 -11)"
                     fill="#000000"
-                    fill-rule="nonzero">
+                    fillRule="nonzero">
                     <path
                       d="m25.576 11.414c0.56558 0.55188 0.56558 1.4439 0 1.9961l-9.404 9.176c-0.28213 0.27529-0.65247 0.41385-1.0228 0.41385-0.37034 0-0.74068-0.13855-1.0228-0.41385l-4.7019-4.588c-0.56584-0.55188-0.56584-1.4442 0-1.9961 0.56558-0.55214 1.4798-0.55214 2.0456 0l3.679 3.5899 8.3812-8.1779c0.56558-0.55214 1.4798-0.55214 2.0456 0z"></path>
                   </g>
                 </g>
               </svg>
             </div>
-            <div class="ml-3 text-sm leading-6">
+            <div className="ml-3 text-sm leading-6">
               <label
-                for="checkbox-1"
-                class="text-sm font-medium text-white">
+                htmlFor="checkbox-1"
+                className="text-sm font-medium text-white">
                 Remember me
               </label>
             </div>
           </div>
-          <p class="cursor-pointer text-sm text-white hover:underline">Forgot password?</p>
+          <p className="cursor-pointer text-sm text-white hover:underline">Forgot password?</p>
         </div>
-        <button
-          class="w-full bg-[#ae7aff] p-3 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e]">
+        <button className="w-full bg-[#ae7aff] p-3 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e]"
+          onClick={handleLogin}
+          >
           Log in
+          
         </button>
-        <div class="mx-auto my-3 flex w-full max-w-md items-center justify-center gap-4 text-white">
-          <hr class="w-full border-[0.1px] border-white" />
-          <p class="text-sm">OR</p>
-          <hr class="w-full border-[0.1px] border-white" />
+        <ToastContainer />
+        <div className="mx-auto my-3 flex w-full max-w-md items-center justify-center gap-4 text-white">
+          <hr className="w-full border-[0.1px] border-white" />
+          <p className="text-sm">OR</p>
+          <hr className="w-full border-[0.1px] border-white" />
         </div>
-        <button class="inline-flex w-full items-center justify-center gap-3 border-[1px] border-white bg-black p-3 text-center text-white">
+        <button className="inline-flex w-full items-center justify-center gap-3 border-[1px] border-white bg-black p-3 text-center text-white">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             height="24"
@@ -99,7 +144,7 @@ function LoginPage() {
           </svg>
           Login with Google
         </button>
-        <button class="inline-flex w-full items-center justify-center gap-3 border-[1px] border-white bg-black p-3 text-center text-white">
+        <button className="inline-flex w-full items-center justify-center gap-3 border-[1px] border-white bg-black p-3 text-center text-white">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -111,15 +156,17 @@ function LoginPage() {
           </svg>
           Login with GitHub
         </button>
-        <p class="my-14 text-sm font-light text-white">
+        <p className="my-14 text-sm font-light text-white">
           Don&#x27;t have an account?
-          <span class="cursor-pointer font-bold hover:underline">Create an account</span>
+          <Link className="cursor-pointer font-bold hover:underline"
+          to='/register'
+          >Create an account</Link>
         </p>
-      </div>
+      </form>
     </div>
-    <div class="fixed right-0 z-20 hidden h-screen w-1/2 md:block">
+    <div className="fixed right-0 z-20 hidden h-screen w-1/2 md:block">
       <img
-        class="h-full w-full object-cover"
+        className="h-full w-full object-cover"
         src="https://images.pexels.com/photos/1144275/pexels-photo-1144275.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
         alt="register_image" />
     </div>
