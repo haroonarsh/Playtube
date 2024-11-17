66a9ef938937e045970594376ba730732aeca25f
Haroon Arshad <arshadharoon216@gmail.com>
2024-11-17 21:53:10 +0500

    some  functionality added

--- frontend/src/components/MyChannel/EmptyPage.jsx
+++ frontend/src/components/MyChannel/EmptyPage.jsx
@@ -520,7 +520,7 @@ function EmptyPage() {
                     strokeLinecap="round"
                     strokeLinejoin="round"
                     d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z"></path>
-                </svg>
+                </svg>``
               </span>
             </p>
             <h5 className="mb-2 font-semibold">No videos uploaded</h5>
